69c9c1aa65a47e73bc715a1a69ee56b45ea9b64c
denisanoshin <mr.denis.anoshin@gmail.com>
2025-04-07 20:15:03 +0300

    fix config

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,5 +1,9 @@
-export const HOST = "localhost";
-export const USER = "listik";
-export const PASSWORD = "root";
-export const DB = "chat";
-export const dialect = "postgres";
+// db.config.js
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const HOST = process.env.DB_HOST || "localhost";
+export const USER = process.env.DB_USER || "listik";
+export const PASSWORD = process.env.DB_PASSWORD || "root";
+export const DB = process.env.DB_NAME || "chat";
+export const dialect = process.env.DB_DIALECT || "postgres";
