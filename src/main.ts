e2547ad841383e887a1e7e11c9168ae3d86a3318
DenisAnoshin <cweic81@gmail.com>
2025-06-10 13:16:26 +0300

    yy

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: true,
     credentials: true,
     methods: '*',
     allowedHeaders: '*',
